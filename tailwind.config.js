d55a078ae964c58e92bf939a8f7596c0dfdabe04
Javier Morón <requin883@gmail.com>
2023-02-10 10:16:38 -0800

    Create tailwind config file updateFontFamily theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'header': ['Futura PT Medium', 'Arial'],
+      'para': ['Futura PT Book', 'Arial']
+    },
     extend: {},
   },
   plugins: [],
